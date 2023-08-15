3290165355b44a52967fe90601ae2623da23284e
rslNoCounter <rasselnr49@gmail.com>
2023-08-15 15:10:55 +0700

    a

---
+++ Jobsheet_5/no_3.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Masukkan sebuah kalimat berisi angka: ", (str) => {
+    let int = parseInt(str);
+    console.log(typeof int);
+    rl.close();
+});
